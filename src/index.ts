42c4db71f95cb39cf9f9c9f174f46520b774310a
Oscar <lagartoverde97@gmail.com>
2024-06-13 13:19:57 +0200

    linting

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 type AlgorithmFunction = () => string;
-type MemoizationEntry = {
+interface MemoizationEntry  {
   uri: string;
   uuidAlgorithm: string;
 };
