201507a184c3909c78eeafcd5448c3daecfcf43e
Elena Poelman <elena.poelman@redpencil.io>
2024-11-15 15:54:57 +0100

    add support for html entities and quote preservation

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 type AlgorithmFunction = () => string;
 interface MemoizationEntry {
-  uri: string;
+  finalUuid: string;
   uuidAlgorithm: string;
 }
 
@@ -12,29 +12,28 @@ const algorithms: Record<string, AlgorithmFunction> = {
 
 function replacingAlgorithm(
   memoizationTable: Record<string, MemoizationEntry>,
-  uriBase: string,
   uuidAlgorithm: string,
   originalUuid: string,
 ) {
-  const memoizationEntry = memoizationTable[`${uriBase}${originalUuid}`];
+  const memoizationEntry = memoizationTable[originalUuid];
   if (memoizationEntry) {
     if (memoizationEntry.uuidAlgorithm !== uuidAlgorithm) {
       console.warn(
-        `You have 2 entries with the same uri base and uuid, but different algorithm, this is probably and error so the result will be the same uuid in both entries. ${uriBase}--ref-<${uuidAlgorithm}>-${originalUuid} and ${uriBase}--ref-<${memoizationEntry.uuidAlgorithm}>-${originalUuid}`,
+        `You have 2 entries with the same uuid, but different algorithms, this is probably and error so the result will be the same uuid in both entries. --ref-<${uuidAlgorithm}>-${originalUuid} and --ref-<${memoizationEntry.uuidAlgorithm}>-${originalUuid}`,
       );
     }
-    return memoizationTable[`${uriBase}${originalUuid}`].uri;
+    return memoizationTable[originalUuid].finalUuid;
   } else {
     if (!algorithms[uuidAlgorithm]) {
       console.warn(`${uuidAlgorithm} was not recognised, using uuid4 instead`);
     }
     const algorithm = algorithms[uuidAlgorithm] || uuidv4;
-    const finalUri = `"${uriBase}${algorithm()}"`;
-    memoizationTable[`${uriBase}${originalUuid}`] = {
-      uri: finalUri,
+    const finalUuid = algorithm();
+    memoizationTable[originalUuid] = {
+      finalUuid,
       uuidAlgorithm,
     };
-    return finalUri;
+    return finalUuid;
   }
 }
 
@@ -43,13 +42,11 @@ function curryReplacingAlgorithm(
 ) {
   return (
     _: unknown,
-    uriBase: string,
     uuidAlgorithm: string,
     originalUuid: string,
   ) => {
     return replacingAlgorithm(
       memoizationTable,
-      uriBase,
       uuidAlgorithm,
       originalUuid,
     );
@@ -60,7 +57,7 @@ export default function instantiateUuids(templateString: string) {
   const memoizationTable = {};
   const replacingAlgorithmBinded = curryReplacingAlgorithm(memoizationTable);
   return templateString.replace(
-    /["']([^"']+)--ref-([^"'-]+)-([^"']+)["']/g,
+    /--ref-([a-zA-Z0-9]+)-([a-zA-Z0-9-]+)/g,
     replacingAlgorithmBinded,
   );
 }
